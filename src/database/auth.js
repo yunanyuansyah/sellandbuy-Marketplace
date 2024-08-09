372cfe0ded3caef39eee9d4df6b393ae7be7147c
Yunan Yuansyah <yunanyuansyah@gmail.com>
2024-08-09 15:08:03 +0700

    auth

--- src/database/auth.js
+++ src/database/auth.js
@@ -5,8 +5,6 @@ mongoose
   .connect(process.env.MONGODB_URI, 
     { useNewUrlParser: true,
       useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 30000, // Increase timeout to 30 seconds
-      socketTimeoutMS: 45000, // Increase socket timeout to 45 seconds 
     })
   .then(() => {
     console.log("mongoose connected for authentication model");
